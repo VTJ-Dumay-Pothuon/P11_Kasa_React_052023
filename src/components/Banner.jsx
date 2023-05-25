e7150fa449f1795bff1567a032c2090a09efadff
VTJ-Dumay-Pothuon <valentin.dumay.pothuon@gmail.com>
2023-05-25 22:29:28 +0200

    I'm very patient, but...

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,22 +1,17 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import '../styles/Banner.scss';
 
-// deploymentPath is null if url is localhost, otherwise it is the name of the repository
-const deploymentPath = process.env.PUBLIC_URL;
-
 const Banner = ({ content }) => {
-    // get the last part of the url (after the last /) and convert it to lowercase
-    const extractedPath = window.location.pathname.split(`${deploymentPath}/?`).pop().toLowerCase().substring(1);
-    const page = extractedPath !== '' ? extractedPath : 'home';
+  const location = useLocation();
+  const extractedPath = location.pathname.substring(1).toLowerCase();
+  const page = extractedPath !== '' ? extractedPath : 'home';
 
-    // same but the slash is conditional thanks to regex
-    return (
-        // the banner--page class determines the background image of the banner
-        // if content is not empty, display it
-        <div className={`banner banner--${page}`}>
-        {content && <h1>{content}</h1>}
-        </div>
-    );
-    }
+  return (
+    <div className={`banner banner--${page}`}>
+      {content && <h1>{content}</h1>}
+    </div>
+  );
+};
 
 export default Banner;
