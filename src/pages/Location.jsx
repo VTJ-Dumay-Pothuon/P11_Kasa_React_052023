950b4fb893ccb8178db4416a1919c0fba6da7881
VTJ-Dumay-Pothuon <valentin.dumay.pothuon@gmail.com>
2023-05-25 23:25:53 +0200

    Use location instead of history

--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, useHistory } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import '../styles/Location.scss';
 import Header from '../components/Header';
 import Carousel from '../components/Carousel';
@@ -12,68 +12,68 @@ import Footer from '../components/Footer';
 import data from '../assets/data/database_mock.json';
 
 const Location = () => {
-  const { id } = useParams();
-  const history = useHistory();
-  const [location, setLocation] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
+    const { id } = useParams();
+    const [location, setLocation] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    // filter the locations array to get the location with the correct id
-    const filteredLocation = data.filter((location) => location.id === id);
-    // if the location is found, set it to location
-    if (filteredLocation.length) {
-      setLocation(filteredLocation[0]);
-    } else {
-      // if the location is not found, redirect to /notfound
-      history.push('/notfound');
+    useEffect(() => {
+        // filter the locations array to get the location with the correct id
+        const filteredLocation = data.filter((location) => location.id === id);
+        // if the location is found, set it to location
+        if (filteredLocation.length) {
+            setLocation(filteredLocation[0]);
+        } else {
+            // if the location is not found, navigate to /notfound
+            navigate('/notfound');
+        }
+        // set isLoading to false
+        setIsLoading(false);
+    }, [id, navigate]);
+
+    if (isLoading || !location) {
+        return null; // Peut-être afficher un indicateur de chargement ici
     }
-    // set isLoading to false
-    setIsLoading(false);
-  }, [id, history]);
 
-  return (
-    <React.Fragment>
-      <Header />
-      {!isLoading && location && (
+    return (
         <React.Fragment>
-          <div className="location">
-            <section className="location__carousel">
-              <Carousel images={location.pictures} />
-            </section>
-            <section className="location__header">
-              <h1>{location.title}</h1>
-              <h2>{location.location}</h2>
-              <div className="location__header__infos">
-                <div className="location__header__infos__tags">
-                  {location.tags.map((tag) => (
-                    <Tag key={tag} tag={tag} />
-                  ))}
-                </div>
-                <div className="location__header__infos__right">
-                  <Rating rating={location.rating} />
-                  <Host host={location.host} />
-                </div>
-              </div>
-            </section>
-            <section className="location__content">
-              <Dropdown title="Description" content={location.description} />
-              <Dropdown
-                title="Équipements"
-                content={
-                  <ul>
-                    {location.equipments.map((equipment) => (
-                      <li key={equipment}>{equipment}</li>
-                    ))}
-                  </ul>
-                }
-              />
-            </section>
-          </div>
-          <Footer />
+            <Header />
+            <div className="location">
+                <section className="location__carousel">
+                    <Carousel images={location.pictures} />
+                </section>
+                <section className="location__header">
+                    <h1>{location.title}</h1>
+                    <h2>{location.location}</h2>
+                    <div className="location__header__infos">
+                        <div className="location__header__infos__tags">
+                            {location.tags.map((tag) => (
+                                <Tag key={tag} tag={tag} />
+                            ))}
+                        </div>
+                        <div className="location__header__infos__right">
+                            <Rating rating={location.rating} />
+                            <Host host={location.host} />
+                        </div>
+                    </div>
+                </section>
+                <section className="location__content">
+                    <Dropdown title="Description" content={location.description} />
+                    <Dropdown
+                        title="Équipements"
+                        content={
+                            <ul>
+                                {location.equipments.map((equipment) => (
+                                    <li key={equipment}>{equipment}</li>
+                                ))}
+                            </ul>
+                        }
+                    />
+                </section>
+            </div>
+            <Footer />
         </React.Fragment>
-      )}
-    </React.Fragment>
-  );
+    );
 };
 
 export default Location;
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
-import { useHistory, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import '../styles/NotFound.scss';
 import Header from '../components/Header';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
-  const history = useHistory();
-  const currentPath = window.location.pathname;
+    const navigate = useNavigate();
 
-  // deploymentPath is null if url is localhost, otherwise it is the name of the repository
-  const deploymentPath = process.env.PUBLIC_URL;
+    // deploymentPath is null if url is localhost, otherwise it is the name of the repository
+    const deploymentPath = process.env.PUBLIC_URL;
 
-  if (currentPath !== `${deploymentPath}/notfound`) {
-    history.push(`${deploymentPath}/notfound`);
-  }
+    if (window.location.pathname !== `${deploymentPath}/notfound`) {
+        navigate('/notfound');
+    }
 
-  return (
-    <React.Fragment>
-      <Header />
-      <div className="not-found">
-        <h1>404</h1>
-        <h2>Oups ! La page que vous demandez n'existe pas.</h2>
-        <Link to={`${deploymentPath}/`}>Retourner sur la page d'accueil</Link>
-      </div>
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <Header />
+            <div className="not-found">
+                <h1>404</h1>
+                <h2>Oups ! La page que vous demandez n'existe pas.</h2>
+                <Link to={`${deploymentPath}/`}>Retourner sur la page d'accueil</Link>
+            </div>
+        </React.Fragment>
+    );
 };
 
 export default NotFound;
