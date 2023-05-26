28cabbc38f30645e2a50f752365b95b56a331da3
VTJ-Dumay-Pothuon <valentin.dumay.pothuon@gmail.com>
2023-05-26 17:54:02 +0200

    Maybe this will do

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ createRoot(document.getElementById('root')).render(
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/location/:id" element={<Location />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
     </Router>
   </React.StrictMode>
