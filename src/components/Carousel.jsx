17bd0e6e357cbdf9814e93caca71865b9aa037e7
VTJ-Dumay-Pothuon <valentin.dumay.pothuon@gmail.com>
2023-05-28 02:58:20 +0200

    Hidden buttons if not photo. Also, fancy cursor

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
 import "../styles/Carousel.scss";
 import arrow from "../assets/images/arrow.svg";
+import pauseCursor from "../assets/images/pause.svg";
 
 const Carousel = ({ images }) => {
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
     const [isTimerPaused, setIsTimerPaused] = useState(false);
 
     useEffect(() => {
+        /// disable if images.length === 1
+        if (images.length === 1) { return }
         // if the timer is not paused, set a timer to go to the next image after 5 seconds
         // clicking on the image pauses the timer
         if (!isTimerPaused) {
@@ -39,12 +42,11 @@ const Carousel = ({ images }) => {
     };
 
     return (
-        <div className="carousel">
+        <div className="carousel" style={{cursor: `url(${isTimerPaused ? pauseCursor : null}), auto`}}>
             <div className="carousel__image" style={{ backgroundImage: `url(${images[currentImageIndex]})` }}
                 onClick={() => setIsTimerPaused(!isTimerPaused)}>
             </div>
-
-            <div className="carousel__buttons">
+            <div className="carousel__buttons" style={{ display: images.length === 1 ? "none" : "flex" }}>
                 <button className="carousel__buttons__previous" onClick={previousImage} disabled={images.length === 1}>
                     <img src={arrow} alt="précédent" className="previous" />
                 </button>
@@ -53,10 +55,11 @@ const Carousel = ({ images }) => {
                 </button>
             </div>
 
-            <div className="carousel__index">
+            <div className="carousel__index" style={{ display: images.length === 1 ? "none" : "block" }}>
             <span className="carousel__index__mobile">
                 {images.map((_, index) => (
-                    <span key={index} className={index === currentImageIndex ? "active" : ""}>•</span>
+                    <span key={index} className={index === currentImageIndex ? "active" : ""}
+                    onClick={() => setCurrentImageIndex(index)}>•</span>
                 ))}
             </span>
             <span className="carousel__index__desktop">{`${currentImageIndex + 1}/${images.length}`}</span>
